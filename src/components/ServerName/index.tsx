022296ff2393dd2dec0a9845c2cfe0023b474385
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio-7.local>
2020-06-27 14:41:38 -0300

    feature: added ServerName

---
+++ src/components/ServerName/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Container, Title, ExpandIcon } from "./styles";
+
+const ServerName: React.FC = () => {
+  return (
+    <Container>
+      <Title>Servidor de teste</Title>
+
+      <ExpandIcon />
+    </Container>
+  );
+};
+
+export default ServerName;
